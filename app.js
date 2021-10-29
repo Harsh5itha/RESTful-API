fcc38c91b9ffa162656889f40412a248f4e25023
Harsh5itha <45786603+Harsh5itha@users.noreply.github.com>
2021-10-29 18:48:07 +0530

    GET, POST and DELETE operations done

---
+++ app.js
@@ -0,0 +1,63 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+//TODO
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+const articleSchema = {
+  title: String,
+  content: String
+}
+const Article = mongoose.model("Article",articleSchema);
+
+app.get("/articles", function(req, res){
+  Article.find(function(err, foundarticles){
+
+    if(!err){
+      res.send(foundarticles);
+    }else{
+      res.send(err);
+    }
+  });
+});
+
+app.post("/articles", function(req, res){
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+  newArticle.save(function(err){
+    if(!err){
+      res.send("Success to add new article!!");
+    }else{
+      res.send(err);
+    }
+  });
+});
+
+app.delete("/articles", function(req, res){
+  Article.deleteMany(function(err){
+    if(!err){
+      res.send("Success in deleting all articles!!");
+    }else{
+      res.send(err);
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
